9f8e47d427ddbb0c57a0b8e06302dee607d52a6e
Sai Sri Laxman Jangala <74813943+jangala-laxman@users.noreply.github.com>
2022-10-01 22:18:46 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -4,14 +4,12 @@ var reset_button = document.getElementById('reset')  //accessing reset button
 var timer = document.getElementById('time')          //accessing time element for displaying timer
 var readings = document.getElementById('readings')   //accessing readings area for displaying the elapsed time
 
+let msec = 0;                                         //counts milli seconds
 let sec = 0;                                         //counts seconds
-let min = 0;                                         //counts mins
-let hour = 0;                                        //counts hours       
+let min = 0;                                        //counts mins       
   
 let interval = null;
-// let oHour = document.getElementById("hours");
-// let oMin = document.getElementById("mins");
-// let oSec = document.getElementById("seconds");
+
 
 window.addEventListener('load', ()=>{                //onloading the window timer sets to 0
     timer.innerHTML = "00:00:00";
@@ -21,56 +19,59 @@ stop_button.addEventListener('click',pause);         //onclicking the stop butto
 reset_button.addEventListener('click',()=>{          //onclicking the reset button, it resets timer to 0 and stops the timer
     timer.innerHTML = "00:00:00";
     clearInterval(interval)
+    msec = 0;                                        //counts milli seconds
+    sec = 0;                                         //counts seconds
+    min = 0; 
 })
 
 
 function clock(){                                    
-    sec++;                                           //sec will increase by 1 for every 1000 ms       
+    msec++;                                           //sec will increase by 1 for every 1000 ms       
+    if(msec == 0)                                     //if msec = 0, it should be in double digits
+    {
+        msec = "00"
+    }
     if(sec == 0)                                     //if sec = 0, it should be in double digits
     {
-        sec = "00"
+        sec = '00';
     }
     if(min == 0)                                     //if min = 0, it should be in double digits
     {
-        min = '00';
-    }
-    if(hour == 0)                                    //if hour = 0, it should be in double digits
-    {
-        hour = "00";
+        min = "00";
     }
 
-    if(sec<10)                                       //if sec <10, it should be in double digits
+    if(msec<10)                                      //if msec <10, it should be in double digits
     {
-        sec = '0' + sec;
+        msec = '0' + msec;
     }
     
-    if(sec == 60)                                    //if sec = 60, it  should start from 0
+    if(msec == 100)                                  //if msec = 100, it  should start from 0
     {
-        sec = 0
-        min++;                                       //incrementing minutes
-        if(min<10)                                   //if min <10, it should be in double digits
+        msec = 0
+        sec++;                                       //incrementing sec
+        if(sec<10)                                   //if sec <10, it should be in double digits
         {
-            min = '0' + min             
+            sec = '0' + sec         
         }
 
-        if(min == 60)                                //if min = 60, it  should start from 0
+        if(sec == 60)                                //if min = 60, it  should start from 0
         {
-            min = 0;
-            hour++;                                  // incrementing hours
-            if(hour<10)                              //if hour <10, it should be in double digits
+            sec = 0;
+            min++;                                   // incrementing mins
+            if(min<10)                               //if min <10, it should be in double digits
             {
-                hour = '0' + hour
+                min = '0' + min
             }
         }
     }
 
 
-    timer.innerHTML = `${hour}:${min}:${sec}`         // displaying the timer dynamically on the page
+    timer.innerHTML = `${min}:${sec}:${msec}`         // displaying the timer dynamically on the page
 }
 
 function starttimer(){                                // function to start the timer using the interval
     clearInterval(interval)                           // clears the timer set with SetInterval method
-    interval = setInterval(clock,1000)
+    interval = setInterval(clock,10)
 }
 
 
